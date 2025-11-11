79d27e8427bfe8077c5ffbe2f30df5187938ca27
Nathan Ziehnert <nathan@z-nerd.com>
2025-11-10 22:31:11 -0600

    Add Azure storage local dependency

--- wywo-webservices/wywo.frontend/Program.cs
+++ wywo-webservices/wywo.frontend/Program.cs
@@ -2,13 +2,21 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wywo.frontend;
 using wywo.frontend.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.Azure;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddScoped<IFlowbiteService, FlowbiteService>();
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+});
 
 await builder.Build().RunAsync();
